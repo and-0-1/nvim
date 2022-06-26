36fb00f8a688e029902be7e86455ae95a39e1cab
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-06-27 10:04:05 +1200

    chore(bufferline): comment out

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -50,7 +50,7 @@ return packer.startup(function(use)
   use "kyazdani42/nvim-web-devicons"
   use { "kyazdani42/nvim-tree.lua" }
   use "tamago324/lir.nvim"
-  use "akinsho/bufferline.nvim"
+  -- use "akinsho/bufferline.nvim"
   use "moll/vim-bbye"
   use "nvim-lualine/lualine.nvim"
   use "akinsho/toggleterm.nvim"
