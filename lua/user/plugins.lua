948934153ede75882f641068ece27d8735b75ee1
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-06-27 10:03:51 +1200

    chore(quickscope): remove

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -59,7 +59,6 @@ return packer.startup(function(use)
   -- use "lukas-reineke/indent-blankline.nvim"
   use "goolord/alpha-nvim"
   use "folke/which-key.nvim"
-  -- use "unblevable/quick-scope"
   use "phaazon/hop.nvim"
   use "andymass/vim-matchup"
   use "nacro90/numb.nvim"
