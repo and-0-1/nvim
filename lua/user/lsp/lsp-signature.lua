4ed4df1a08baf78082f9681a659f4277c95e1958
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-06-30 11:46:36 +1200

    chore(lsp): cleanup

--- lua/user/lsp/lsp-signature.lua
+++ lua/user/lsp/lsp-signature.lua
@@ -50,8 +50,3 @@ local cfg = {
 
 -- recommanded:
 signature.setup(cfg) -- no need to specify bufnr if you don't use toggle_key
-
--- You can also do this inside lsp on_attach
--- note: on_attach deprecated
--- require("lsp_signature").on_attach(cfg, bufnr) -- no need to specify bufnr if you don't use toggle_key
--- signature.on_attach(cfg) -- no need to specify bufnr if you don't use toggle_key
