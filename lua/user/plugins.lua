9a61b01c9e1ca6cf83e1aa49bb5767861c375fed
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-06-30 11:18:40 +1200

    chore(neozoom): remove

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -77,7 +77,6 @@ return packer.startup(function(use)
   use "tpope/vim-abolish"
   use "rcarriga/nvim-notify"
   use "tversteeg/registers.nvim"
-  -- use "nyngwang/NeoZoom.lua"
   use "SmiteshP/nvim-gps"
   use {
     "iamcco/markdown-preview.nvim",
