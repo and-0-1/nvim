bb38070536f16b0247d0b189daf0b5fa5faa92df
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-07-04 09:54:55 +1200

    feat(telescope): q to close in normal mode

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -49,6 +49,7 @@ telescope.setup {
 
       n = {
         ["<esc>"] = actions.close,
+        ["q"] = actions.close,
         ["<CR>"] = actions.select_default,
         ["<C-x>"] = actions.select_horizontal,
         ["<C-v>"] = actions.select_vertical,
