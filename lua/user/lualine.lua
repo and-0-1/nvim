885ef3e96ebecc131ff4df659c47bd06725496e2
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-07-06 11:00:07 +1200

    chore(lualine): simplify config; use auto for theme

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -3,52 +3,9 @@ if not status_ok then
   return
 end
 
-local status_theme_ok, theme = pcall(require, "lualine.themes.darkplus_dark")
-if not status_theme_ok then
-  return
-end
-
-vim.api.nvim_set_hl(0, "SLGitIcon", { fg = "#E8AB53", bg = "#303030" })
-vim.api.nvim_set_hl(0, "SLBranchName", { fg = "#D4D4D4", bg = "#303030", bold = false })
--- vim.api.nvim_set_hl(0, "SLProgress", { fg = "#D7BA7D", bg = "#252525" })
-vim.api.nvim_set_hl(0, "SLProgress", { fg = "#D4D4D4", bg = "#303030" })
-vim.api.nvim_set_hl(0, "SLSeparator", { fg = "#808080", bg = "#252525" })
-local mode_color = {
-  n = "#569cd6",
-  i = "#6a9955",
-  v = "#c586c0",
-  [""] = "#c586c0",
-  V = "#c586c0",
-  -- c = '#B5CEA8',
-  -- c = '#D7BA7D',
-  c = "#4EC9B0",
-  no = "#569cd6",
-  s = "#ce9178",
-  S = "#ce9178",
-  [""] = "#ce9178",
-  ic = "#dcdcaa",
-  R = "#d16969",
-  Rv = "#d16969",
-  cv = "#569cd6",
-  ce = "#569cd6",
-  r = "#d16969",
-  rm = "#4EC9B0",
-  ["r?"] = "#4EC9B0",
-  ["!"] = "#4EC9B0",
-  t = "#D7BA7D",
-}
-
 local mode = {
-  -- mode component
-  function()
-    return "▊"
-  end,
-  color = function()
-    -- auto change color according to neovims mode
-    return { fg = mode_color[vim.fn.mode()] }
-  end,
-  -- padding = { right = 1 },
-  padding = 0,
+  "mode",
+  padding = { left = 1, right = 1 },
 }
 
 local hide_in_width = function()
@@ -69,63 +26,29 @@ local diagnostics = {
 
 local diff = {
   "diff",
-  colored = false,
+  -- colored = false,
   symbols = { added = icons.git.Add .. " ", modified = icons.git.Mod .. " ", removed = icons.git.Remove .. " " }, -- changes diff symbols
   cond = hide_in_width,
-  separator = "%#SLSeparator#" .. "│ " .. "%*",
+  separator = "|",
+  padding = 1,
 }
-
--- local mode = {
---   "mode",
---   fmt = function(str)
---     return "-- " .. str .. " --"
---   end,
--- }
 
 local filetype = {
   "filetype",
   icons_enabled = true,
-  -- icon = nil,
 }
 
 local branch = {
   "branch",
   icons_enabled = true,
   icon = "%#SLGitIcon#" .. "" .. "%*" .. "%#SLBranchName#",
-  -- color = "Constant",
-  colored = false,
+  -- colored = false,
 }
 
 local progress = {
   "progress",
   color = "SLProgress",
-  -- fmt = function(str)
-  --   print(vim.fn.expand(str))
-  --   if str == "1%" then
-  --     return "TOP"
-  --   end
-  --   if str == "100%" then
-  --     return "BOT"
-  --   end
-  --   return str
-  -- end,
-  -- padding = 0,
 }
-
--- local progress = {
---   "progress",
---   fmt = function(str)
---     print(vim.fn.expand(str))
---     if str == "1%" then
---       return "TOP"
---     end
---     if str == "100%" then
---       return "BOT"
---     end
---     return str
---   end,
---   -- padding = 0,
--- }
 
 local current_signature = function()
   if not pcall(require, "lsp_signature") then
@@ -133,41 +56,26 @@ local current_signature = function()
   end
   local sig = require("lsp_signature").status_line(30)
   -- return sig.label .. "🐼" .. sig.hint
-  return "%#SLSeparator#" .. sig.hint .. "%*"
+  return sig.hint
 end
-
--- cool function for progress
--- local progress = function()
---   local current_line = vim.fn.line "."
---   local total_lines = vim.fn.line "$"
---   local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
---   local line_ratio = current_line / total_lines
---   local index = math.ceil(line_ratio * #chars)
---   -- return chars[index]
---   return "%#SLProgress#" .. chars[index] .. "%*"
--- end
 
 local spaces = {
   function()
     return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
   end,
-  padding = 0,
-  separator = "%#SLSeparator#" .. " │" .. "%*",
+  separator = "|",
+  padding = 1,
 }
 
 local location = {
   "location",
-  color = function()
-    return { fg = "#252525", bg = mode_color[vim.fn.mode()] }
-  end,
 }
 
 lualine.setup {
   options = {
     globalstatus = true,
     icons_enabled = true,
-    -- theme = "auto",
-    theme = theme,
+    theme = "auto",
     component_separators = { left = "", right = "" },
     section_separators = { left = "", right = "" },
     disabled_filetypes = { "alpha", "dashboard" },
