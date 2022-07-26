14d256c0f4b32b6315facb13d897f857fdabe140
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-07-27 09:47:23 +1200

    chore(bufferline): nvimtree; allow for unique names

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -12,29 +12,12 @@ keymap("n", "L", "<cmd>BufferLineCycleNext<cr>", keymap_opts)
 bufferline.setup {
   options = {
     numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-    -- indicator_icon = "▎",
-    -- buffer_close_icon = '',
-    -- modified_icon = "●",
-    -- close_icon = "",
-    -- close_icon = '',
-    -- left_trunc_marker = "",
-    -- right_trunc_marker = "",
-    --- name_formatter can be used to change the buffer's label in the bufferline.
-    --- Please note some names can/will break the
-    --- bufferline so use this at your discretion knowing that it has
-    --- some limitations that will *NOT* be fixed.
-    -- name_formatter = function(buf)  -- buf contains a "name", "path" and "bufnr"
-    --   -- remove extension from markdown files for example
-    --   if buf.name:match('%.md') then
-    --     return vim.fn.fnamemodify(buf.name, ':t:r')
-    --   end
-    -- end,
     max_name_length = 30,
     max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
     tab_size = 21,
     diagnostics = "nvim_lsp", -- | "nvim_lsp" | "coc",
     diagnostics_update_in_insert = false,
-    offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+    offsets = { { filetype = "NvimTree", text = "NvimTree" } },
     show_buffer_icons = true,
     show_buffer_close_icons = false,
     show_close_icon = false,
@@ -43,7 +26,7 @@ bufferline.setup {
     -- can also be a table containing 2 custom separators
     -- [focused and unfocused]. eg: { '|', '|' }
     separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
-    enforce_regular_tabs = true,
+    enforce_regular_tabs = false,
     always_show_bufferline = false,
     -- sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
     --   -- add custom logic
