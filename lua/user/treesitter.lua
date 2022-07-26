95aebe415f40b4e07180da896767d36398781b3a
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-07-27 09:37:15 +1200

    chore(treesitter): matchup and other options

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -10,6 +10,12 @@ configs.setup {
   ensure_installed = "all", -- one of "all" or a list of languages
   sync_install = true, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
+  matchup = {
+    enable = true, -- mandatory, false will disable the whole extension
+    -- disable = { "c", "ruby" }, -- optional, list of language that will be disabled
+    -- disable_virtual_text = false,
+    -- include_match_words = false
+  },
   highlight = {
     -- use_languagetree = true,
     enable = true, -- false will disable the whole extension
@@ -20,7 +26,9 @@ configs.setup {
   autopairs = {
     enable = true,
   },
-  indent = { enable = true, disable = { "python", "css" } },
+  indent = {
+    enable = true, --[[ disable = { "python", "css" } ]]
+  },
   context_commentstring = {
     enable = true,
     enable_autocmd = false,
