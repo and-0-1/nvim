8d2cc942f98ff99259426f87900e1f3cc10b7ab3
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-07-27 09:35:26 +1200

    chore(autocommands): add groups to prevent reattaching

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -8,6 +8,7 @@
 -- })
 
 vim.api.nvim_create_autocmd({ "User" }, {
+  group = vim.api.nvim_create_augroup("AndoAlphaReady", { clear = true }),
   pattern = { "AlphaReady" },
   callback = function()
     vim.cmd [[
@@ -17,6 +18,7 @@ vim.api.nvim_create_autocmd({ "User" }, {
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = vim.api.nvim_create_augroup("AndoQForExit", { clear = true }),
   pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir", "DressingSelect" },
   callback = function()
     vim.cmd [[
@@ -27,6 +29,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = vim.api.nvim_create_augroup("AndoMDOpts", { clear = true }),
   pattern = { "gitcommit", "markdown" },
   callback = function()
     vim.opt_local.wrap = true
@@ -35,6 +38,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = vim.api.nvim_create_augroup("AndoLIROpts", { clear = true }),
   pattern = { "lir" },
   callback = function()
     vim.opt_local.number = false
@@ -43,6 +47,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  group = vim.api.nvim_create_augroup("AndoTermOpts", { clear = true }),
   pattern = { "term://*" },
   callback = function()
     vim.cmd "startinsert!"
@@ -60,51 +65,60 @@ vim.cmd "autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTre
 -- })
 
 vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = vim.api.nvim_create_augroup("AndoWinResize", { clear = true }),
   callback = function()
     vim.cmd "tabdo wincmd ="
   end,
 })
 
 vim.api.nvim_create_autocmd({ "CmdWinEnter" }, {
+  group = vim.api.nvim_create_augroup("AndoWinEnter", { clear = true }),
   callback = function()
     vim.cmd "quit"
   end,
 })
 
 if vim.fn.has "nvim-0.8" == 1 then
-  vim.api.nvim_create_autocmd({ "CursorMoved", "CursorHold", "BufWinEnter", "BufFilePost", "InsertEnter", "BufWritePost" }, {
-    callback = function()
-      require("user.winbar").get_winbar()
-    end,
-  })
+  vim.api.nvim_create_autocmd(
+    { "CursorMoved", "CursorHold", "BufWinEnter", "BufFilePost", "InsertEnter", "BufWritePost" },
+    {
+      group = vim.api.nvim_create_augroup("AndoWinbar", { clear = true }),
+      callback = function()
+        require("user.winbar").get_winbar()
+      end,
+    }
+  )
 end
 
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  group = vim.api.nvim_create_augroup("AndoCRO", { clear = true }),
   callback = function()
     vim.cmd "set formatoptions-=cro"
   end,
 })
 
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+  group = vim.api.nvim_create_augroup("AndoYank", { clear = true }),
   callback = function()
     vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
   end,
 })
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  pattern = { "*.java" },
-  callback = function()
-    vim.lsp.codelens.refresh()
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+--   pattern = { "*.java" },
+--   callback = function()
+--     vim.lsp.codelens.refresh()
+--   end,
+-- })
 
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-  callback = function()
-    vim.cmd "hi link illuminatedWord LspReferenceText"
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "VimEnter" }, {
+--   callback = function()
+--     vim.cmd "hi link illuminatedWord LspReferenceText"
+--   end,
+-- })
 
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  group = vim.api.nvim_create_augroup("AndoCheckTime", { clear = true }),
   pattern = { "*" },
   callback = function()
     vim.cmd "checktime"
