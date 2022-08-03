2198b0c333f5d127b338bb6140bf6edec3c5bb1a
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-08-04 09:50:21 +1200

    chore(whichkey): change to <leader>lI to mason

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -193,7 +193,7 @@ local mappings = {
     },
     F = { "<cmd>LspToggleAutoFormat<cr>", "Toggle Autoformat" },
     i = { "<cmd>LspInfo<cr>", "Info" },
-    I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
+    I = { "<cmd>Mason<cr>", "Installer Info" },
     j = {
       "<cmd>lua vim.diagnostic.goto_next({buffer=0})<CR>",
       "Next Diagnostic",
