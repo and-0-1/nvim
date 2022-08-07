98286757143ed2a668f4021a0ee4e88ca1d8673b
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-08-07 18:21:08 +1200

    chore(matchup): disable popup; handle through TS

--- lua/user/matchup.lua
+++ lua/user/matchup.lua
@@ -1,3 +1,3 @@
 vim.g.matchup_enabled = 1
-vim.g.matchup_matchparen_offscreen = { method = 'popup' }
+-- vim.g.matchup_matchparen_offscreen = { method = 'popup' }
 -- vim.g.matchup_matchpref = { html = { nolists = 1 } }
