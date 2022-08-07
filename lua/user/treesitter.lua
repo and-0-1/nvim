5139b2f9a98d32f9527cb5f0deb2edfbe7fc8677
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-08-07 18:19:36 +1200

    feat(treesitter): textobject bindings

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -13,7 +13,7 @@ configs.setup {
   matchup = {
     enable = true, -- mandatory, false will disable the whole extension
     -- disable = { "c", "ruby" }, -- optional, list of language that will be disabled
-    -- disable_virtual_text = false,
+    disable_virtual_text = true,
     -- include_match_words = false
   },
   highlight = {
@@ -39,56 +39,93 @@ configs.setup {
   },
   rainbow = {
     enable = true,
-    -- colors = {
-    --   "Gold",
-    --   "Orchid",
-    --   "DodgerBlue",
-    --   -- "Cornsilk",
-    --   -- "Salmon",
-    --   -- "LawnGreen",
-    -- },
-    disable = { "html" },
+    -- disable = { "html" },
   },
   playground = {
     enable = false,
   },
   -- TODO: look into this in more detail; navigating with treesitter objects; add good mappings
-  -- textobjects = {
-  --   select = {
-  --     enable = true,
-  --
-  --     -- Automatically jump forward to textobj, similar to targets.vim
-  --     lookahead = true,
-  --
-  --     keymaps = {
-  --       -- You can use the capture groups defined in textobjects.scm
-  --       ["af"] = "@function.outer",
-  --       ["if"] = "@function.inner",
-  --       ["ac"] = "@class.outer",
-  --       ["ic"] = "@class.inner",
-  --       ["aa"] = "@parameter.outer",
-  --       ["ia"] = "@parameter.inner",
-  --     },
-  --   },
-  --   move = {
-  --     enable = true,
-  --     set_jumps = true, -- whether to set jumps in the jumplist
-  --     goto_next_start = {
-  --       ["]m"] = "@function.outer",
-  --       ["]]"] = "@class.outer",
-  --     },
-  --     goto_next_end = {
-  --       ["]M"] = "@function.outer",
-  --       ["]["] = "@class.outer",
-  --     },
-  --     goto_previous_start = {
-  --       ["[m"] = "@function.outer",
-  --       ["[["] = "@class.outer",
-  --     },
-  --     goto_previous_end = {
-  --       ["[M"] = "@function.outer",
-  --       ["[]"] = "@class.outer",
-  --     },
-  --   },
-  -- },
+  textobjects = {
+    select = {
+      enable = true,
+
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ab"] = "@block.outer",
+        ["ib"] = "@block.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+        ["aa"] = "@parameter.outer",
+        ["ia"] = "@parameter.inner",
+        ["al"] = "@loop.outer",
+        ["il"] = "@loop.inner",
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        [")f"] = "@function.outer",
+        [")b"] = "@block.outer",
+        [")c"] = "@class.outer",
+        [")a"] = "@parameter.outer",
+        [")l"] = "@loop.outer",
+        -- ["]f"] = "@function.inner",
+        -- ["]b"] = "@block.inner",
+        -- ["]c"] = "@class.inner",
+        -- ["]a"] = "@parameter.inner",
+        -- ["]l"] = "@loop.inner",
+      },
+      goto_next_end = {
+        [")F"] = "@function.outer",
+        [")B"] = "@block.outer",
+        [")C"] = "@class.outer",
+        [")A"] = "@parameter.outer",
+        [")L"] = "@loop.outer",
+        -- ["]F"] = "@function.inner",
+        -- ["]B"] = "@block.inner",
+        -- ["]C"] = "@class.inner",
+        -- ["]A"] = "@parameter.inner",
+        -- ["]L"] = "@loop.inner",
+      },
+      goto_previous_start = {
+        ["(f"] = "@function.outer",
+        ["(b"] = "@block.outer",
+        ["(c"] = "@class.outer",
+        ["(a"] = "@parameter.outer",
+        ["(l"] = "@loop.outer",
+        -- ["[f"] = "@function.inner",
+        -- ["[b"] = "@block.inner",
+        -- ["[c"] = "@class.inner",
+        -- ["[a"] = "@parameter.inner",
+        -- ["[l"] = "@loop.inner",
+      },
+      goto_previous_end = {
+        ["(F"] = "@function.outer",
+        ["(B"] = "@block.outer",
+        ["(C"] = "@class.outer",
+        ["(A"] = "@parameter.outer",
+        ["(L"] = "@loop.outer",
+        -- ["[F"] = "@function.inner",
+        -- ["[B"] = "@block.inner",
+        -- ["[C"] = "@class.inner",
+        -- ["[A"] = "@parameter.inner",
+        -- ["[L"] = "@loop.inner",
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>a"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>A"] = "@parameter.inner",
+      },
+    },
+  },
 }
