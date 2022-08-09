9a94b8d00629134f55f2ac4ed021263a4d249929
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-08-09 17:26:51 +1200

    chore(todo-comments): cleanup

--- lua/user/todo-comments.lua
+++ lua/user/todo-comments.lua
@@ -44,15 +44,6 @@ todo_comments.setup {
     max_line_len = 400, -- ignore lines longer than this
     exclude = {}, -- list of file types to exclude highlighting
   },
-  -- list of named colors where we try to extract the guifg from the
-  -- list of hilight groups or use the hex color if hl not found as a fallback
-  -- colors = {
-  --   error = { "LspDiagnosticsDefaultError", "ErrorMsg", "#DC2626" },
-  --   warning = { "LspDiagnosticsDefaultWarning", "WarningMsg", "#FBBF24" },
-  --   info = { "LspDiagnosticsDefaultInformation", "#2563EB" },
-  --   hint = { "LspDiagnosticsDefaultHint", "#10B981" },
-  --   default = { "Identifier", "#7C3AED" },
-  -- },
   search = {
     command = "rg",
     args = {
