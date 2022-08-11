80786db02efb53cec8370e3f499e8c99bad0f66d
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-08-11 17:02:01 +1200

    chore(whichkey): cleanup

--- lua/user/whichkey/init.lua
+++ lua/user/whichkey/init.lua
@@ -5,13 +5,17 @@ end
 
 local wk_settings = require "user.whichkey.settings"
 
+which_key.setup(wk_settings.setup)
+
 local mappings = {
+  -- general
   w = { "<cmd>w<CR>", "Write" },
   h = { "<cmd>nohlsearch<CR>", "No HL" },
   q = { '<cmd>lua require("user.functions").smart_quit()<CR>', "Quit" },
   zz = { '<cmd>lua require("user.functions").save_and_exit()<CR>', "Save and exit" },
   c = { "<cmd>Bdelete!<CR>", "Close Buffer" },
 
+  -- options
   o = {
     name = "Options",
     w = { '<cmd>lua require("user.functions").toggle_option("wrap")<cr>', "Wrap" },
@@ -21,6 +25,7 @@ local mappings = {
     t = { '<cmd>lua require("user.functions").toggle_tabline()<cr>', "Tabline" },
   },
 
+  -- window splitting
   ["-"] = { "<cmd>split<cr>", "HSplit" },
   ["|"] = { "<cmd>vsplit<cr>", "VSplit" },
 
@@ -29,6 +34,7 @@ local mappings = {
     g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
   },
 
+  -- TODO: maybe move these to lsp?
   l = {
     name = "LSP",
     c = { "<cmd>lua require('user.lsp').server_capabilities()<cr>", "Get Capabilities" },
@@ -60,6 +66,4 @@ local mappings = {
   },
 }
 
-which_key.setup(wk_settings.setup)
 which_key.register(mappings, wk_settings.opts)
-which_key.register(vmappings, wk_settings.vopts)
