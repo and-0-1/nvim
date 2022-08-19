6a6907a89a57136ad0435c17dec65b500843b4f2
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-08-19 15:05:27 +1200

    chore(comment): upgrades, breaking changes

--- lua/user/comment/keymaps.lua
+++ lua/user/comment/keymaps.lua
@@ -6,11 +6,11 @@ end
 local wk_settings = require "user.whichkey.settings"
 
 local mappings = {
-  ["/"] = { '<cmd>lua require("Comment.api").toggle_current_linewise()<CR>', "Comment" },
+  ["/"] = { '<cmd>lua require("Comment.api").toggle.linewise.current()<CR>', "Comment" },
 }
 
 local vmappings = {
-  ["/"] = { '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>', "Comment" },
+  ["/"] = { '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>', "Comment" },
 }
 
 which_key.register(mappings, wk_settings.opts)
