67f84c689c80b4b951f5d06694a0f12b1e6903ac
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-09-01 14:11:04 +1200

    chore(true-zen): use defaults for now

--- lua/user/true-zen/init.lua
+++ lua/user/true-zen/init.lua
@@ -27,17 +27,17 @@ zen.setup {
     ataraxis = {
       shade = "dark", -- if `dark` then dim the padding windows, otherwise if it's `light` it'll brighten said windows
       backdrop = 0, -- percentage by which padding windows should be dimmed/brightened. Must be a number between 0 and 1. Set to 0 to keep the same background color
-      minimum_writing_area = { -- minimum size of main window
-        width = 70,
-        height = 44,
-      },
+      -- minimum_writing_area = { -- minimum size of main window
+      --   width = 70,
+      --   height = 44,
+      -- },
       quit_untoggles = true, -- type :q or :qa to quit Ataraxis mode
-      padding = { -- padding windows
-        left = 30,
-        right = 30,
-        top = 0,
-        bottom = 0,
-      },
+      -- padding = { -- padding windows
+      --   left = 30,
+      --   right = 30,
+      --   top = 0,
+      --   bottom = 0,
+      -- },
       callbacks = callbacks,
     },
     minimalist = {
