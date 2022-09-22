235304245f3c4cf9e529960ab6d3aa9d9854f162
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-09-23 09:33:09 +1200

    chore: cleanup

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -9,7 +9,7 @@ local options = {
   fileencoding = "utf-8", -- the encoding written to a file
   hlsearch = true, -- highlight all matches on previous search pattern
   ignorecase = true, -- ignore case in search patterns
-  mouse = "a", -- allow the mouse to be used in neovim
+  -- mouse = "a", -- allow the mouse to be used in neovim
   pumheight = 10, -- pop up menu height
   showmode = false, -- we don't need to see things like -- INSERT -- anymore
   showtabline = 0, -- always show tabs
@@ -28,10 +28,10 @@ local options = {
   tabstop = 4, -- insert 2 spaces for a tab
   cursorline = true, -- highlight the current line
   number = true, -- set numbered lines
+  relativenumber = true, -- set relative numbered lines
   laststatus = 3,
   showcmd = false,
   ruler = false,
-  relativenumber = true, -- set relative numbered lines
   numberwidth = 4, -- set number column width to 2 {default 4}
   signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
   wrap = false, -- display lines as one long line
@@ -54,9 +54,9 @@ for k, v in pairs(options) do
   vim.opt[k] = v
 end
 
-vim.cmd "set whichwrap+=<,>,[,],h,l"
-vim.cmd [[set iskeyword+=-]]
-vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+-- vim.cmd "set whichwrap+=<,>,[,],h,l"
+-- vim.cmd [[set iskeyword+=-]]
+-- vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
 
 -- vim.filetype.add {
 --   extension = {
