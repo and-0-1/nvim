270d06ee0879cbd7f1c8199b6036dedeb4d6c1ee
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-10-17 12:18:45 +1300

    fix(windows): port to new pattern

--- lua/user/windows.lua
+++ lua/user/windows.lua
@@ -3,7 +3,22 @@ if not status_ok then
   return
 end
 
-vim.o.winwidth = 10
-vim.o.winminwidth = 10
-vim.o.equalalways = false
-windows.setup()
+windows.setup {
+  autowidth = { --		     |windows.autowidth|
+    enable = true,
+    winwidth = 5, --		      |windows.winwidth|
+    filetype = { --	    |windows.autowidth.filetype|
+      help = 2,
+    },
+  },
+  ignore = { --			|windows.ignore|
+    buftype = { "quickfix", "toggleterm", "term://*" },
+    filetype = { "NvimTree", "neo-tree", "undotree", "gundo", "toggleterm", "term://*" },
+  },
+  animation = {
+    enable = true,
+    duration = 300,
+    fps = 30,
+    easing = "in_out_sine",
+  },
+}
