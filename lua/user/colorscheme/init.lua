81fa04d14eecc3274cee8460abb867f7491e9af7
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-10-19 15:24:33 +1300

    chore: cleanup

--- lua/user/colorscheme/init.lua
+++ lua/user/colorscheme/init.lua
@@ -2,7 +2,6 @@
 require "user.colorscheme.rose-pine"
 
 local colorscheme = "rose-pine"
--- vim.g.tokyonight_style = "night"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
