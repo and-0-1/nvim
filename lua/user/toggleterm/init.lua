16af202ded021ea6c943ce3140f8cd8fb996883d
Andoni Arbulu Lozano <andoni0305@gmail.com>
2022-11-04 16:32:24 +1300

    chore(toggleterm): switch to btop

--- lua/user/toggleterm/init.lua
+++ lua/user/toggleterm/init.lua
@@ -56,7 +56,7 @@ function _LAZYGIT_TOGGLE()
   lazygit:toggle()
 end
 
-local htop = Terminal:new { cmd = "bpytop", hidden = true }
+local htop = Terminal:new { cmd = "btop", hidden = true }
 
 function _HTOP_TOGGLE()
   htop:toggle()
