e08a6e273dcdb4efde848256e23699d11d814006
Andoni Arbulu Lozano <andoni0305@gmail.com>
2023-01-24 08:37:28 +1300

    fix(colorscheme): set termguicolors on init

--- lua/user/colorscheme/init.lua
+++ lua/user/colorscheme/init.lua
@@ -1,6 +1,8 @@
+vim.opt.termguicolors = true
 -- colorscheme settings
 -- require "user.colorscheme.rose-pine"
 -- require "user.colorscheme.mellow"
 -- require "user.colorscheme.tokyonight"
 -- require "user.colorscheme.catpuccin"
-require "user.colorscheme.melange"
+-- require "user.colorscheme.melange"
+require "user.colorscheme.nordic"
