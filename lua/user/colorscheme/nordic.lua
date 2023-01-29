66038d65bdb582c9f194da80388ba21c2f7ae9bb
Andoni Arbulu Lozano <andoni0305@gmail.com>
2023-01-30 09:44:26 +1300

    chore(colorscheme): nordic -- add setup values

--- lua/user/colorscheme/nordic.lua
+++ lua/user/colorscheme/nordic.lua
@@ -3,4 +3,31 @@ if not status_ok then
   return
 end
 
+local palette = require "nordic.colors"
+
+nordic.setup {
+  -- Telesccope custom configs.
+  telescope = {
+    -- Available styles: `classic`, `flat`.
+    style = "flat",
+  },
+  -- Enable bold keywords.
+  bold_keywords = true,
+  -- Enable italicized comments.
+  italic_comments = true,
+  -- Enable general editor background transparency.
+  transparent_bg = false,
+  -- Override styling of any highlight group.
+  -- (see next section for an example)
+  override = {
+    CursorLineNr = { fg = palette.yellow.dim },
+    LineNr = { fg = palette.gray5 },
+  },
+  cursorline = {
+    bold = true,
+    -- Available themes: 'dark', 'light'.
+    theme = "dark",
+  },
+}
+
 nordic.load()
