ce2d807575341c43d89a7ed94dc2125ce2978820
Andoni Arbulu Lozano <andoni0305@gmail.com>
2023-01-31 16:17:04 +1300

    fix(options): load at beginning

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -39,10 +39,12 @@ local options = {
   sidescrolloff = 8,
   guifont = "monospace:h17", -- the font used in graphical neovim applications
 }
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- vim.opt.fillchars = vim.opt.fillchars + "vertleft: "
 -- vim.opt.fillchars = vim.opt.fillchars + "vertright: "
+
 vim.opt.fillchars = vim.opt.fillchars + "eob: "
 vim.opt.fillchars:append {
   stl = " ",
