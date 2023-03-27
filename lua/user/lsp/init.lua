3398b7dbddbbd5b4db726317be246e111e5302b8
Andoni Arbulu Lozano <andoni0305@gmail.com>
2023-03-28 09:21:00 +1300

    chore(lsp):cleanup

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -3,26 +3,6 @@ if not status_ok then
   return
 end
 
-M = {}
-
-M.server_capabilities = function()
-  local active_clients = vim.lsp.get_active_clients()
-  local active_client_map = {}
-
-  for index, value in ipairs(active_clients) do
-    active_client_map[value.name] = index
-  end
-
-  vim.ui.select(vim.tbl_keys(active_client_map), {
-    prompt = "Select client:",
-    format_item = function(item)
-      return "capabilites for: " .. item
-    end,
-  }, function(choice)
-    vim.pretty_print(vim.lsp.get_active_clients()[active_client_map[choice]].server_capabilities)
-  end)
-end
-
 require "user.lsp.lsp-signature"
 require "user.lsp.mason"
 require("user.lsp.handlers").setup()
@@ -34,5 +14,3 @@ if not l_status_ok then
 end
 
 lsp_lines.setup()
-
-return M
