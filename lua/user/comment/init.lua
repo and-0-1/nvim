afbed5209ebe9d0c1ae4ff8e6bb84f8759981d4f
Andoni Arbulu Lozano <andoni0305@gmail.com>
2023-07-09 12:32:10 +1200

    chore(comment): keymaps

--- lua/user/comment/init.lua
+++ lua/user/comment/init.lua
@@ -34,4 +34,10 @@ comment.setup {
   end,
 }
 
-require "user.comment.keymaps"
+
+local Remap = require "user.keymaps.bind"
+local nnoremap = Remap.nnoremap
+local vnoremap = Remap.vnoremap
+
+nnoremap("<leader>/", '<cmd>lua require("Comment.api").toggle.linewise.current()<CR>')
+vnoremap("<leader>/", '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>')
