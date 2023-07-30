00901ad9a85f58b67e569f8c3db014fbbc7b2eb9
Andoni Arbulu Lozano <andoni0305@gmail.com>
2023-07-31 10:50:57 +1200

    chore(notify): cleanup

--- lua/user/notify.lua
+++ lua/user/notify.lua
@@ -9,12 +9,6 @@ notify.setup {
   -- Animation style (see below for details)
   stages = "fade_in_slide_out",
 
-  -- Function called when a new window is opened, use for changing win settings/config
-  on_open = nil,
-
-  -- Function called when a window is closed
-  on_close = nil,
-
   -- Render function for notifications. See notify-render()
   render = "minimal",
 
