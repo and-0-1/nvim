079c160892241696a844f648b0d03535099dcd5a
Andoni Arbulu Lozano <andoni0305@gmail.com>
2023-08-31 09:48:59 +1200

    feat(dap): add new attach service

--- lua/user/dap/settings/java.lua
+++ lua/user/dap/settings/java.lua
@@ -11,4 +11,14 @@ dap.configurations.java = {
     processId = "${command:PickJavaProcess}",
     cwd = "${workspaceFolder}/mailer",
   },
+  {
+    type = "java",
+    request = "attach",
+    name = "Attach to template-service",
+    hostName = "template-service",
+    port = 8802,
+    projectName = "template-service",
+    processId = "${command:PickJavaProcess}",
+    cwd = "${workspaceFolder}/template-service",
+  },
 }
