c9b0445b8024297f56a5b82b78e71d9989b9170a
Andoni Arbulu Lozano <andoni0305@gmail.com>
2023-11-29 10:11:31 +1300

    chore(comment): remove unsupported

--- lua/user/comment/init.lua
+++ lua/user/comment/init.lua
@@ -3,37 +3,7 @@ if not status_ok then
   return
 end
 
-comment.setup {
-  pre_hook = function(ctx)
-    require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
-
-    if
-      vim.bo.filetype == "typescriptreact"
-      or vim.bo.filetype == "javascriptreact"
-      or vim.bo.filetype == "javascript"
-      or vim.bo.filetype == "typescript"
-    then
-      local U = require "Comment.utils"
-
-      -- Determine whether to use linewise or blockwise commentstring
-      local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
-
-      -- Determine the location where to calculate commentstring from
-      local location = nil
-      if ctx.ctype == U.ctype.blockwise then
-        location = require("ts_context_commentstring.utils").get_cursor_location()
-      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-        location = require("ts_context_commentstring.utils").get_visual_start_location()
-      end
-
-      return require("ts_context_commentstring.internal").calculate_commentstring {
-        key = type,
-        location = location,
-      }
-    end
-  end,
-}
-
+comment.setup {}
 
 local Remap = require "user.keymaps.bind"
 local nnoremap = Remap.nnoremap
