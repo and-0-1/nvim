ff79ecc941e3d2c917c6babaefddfaffa2437dcd
Andoni Arbulu Lozano <andoni0305@gmail.com>
2023-12-08 11:16:07 +1300

    chore(autocommands): remove unneeded commands; replace with ftplugin

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,36 +1,11 @@
 vim.api.nvim_create_autocmd({ "FileType" }, {
   group = vim.api.nvim_create_augroup("AndoQForExit", { clear = true }),
-  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir", "DressingSelect" },
+  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "DressingSelect" },
   callback = function()
     vim.cmd [[
       nnoremap <silent> <buffer> q :close<CR> 
       set nobuflisted 
     ]]
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  group = vim.api.nvim_create_augroup("AndoMDOpts", { clear = true }),
-  pattern = { "gitcommit", "markdown" },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-  end,
-})
-
--- vim.cmd "autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
-
-vim.api.nvim_create_autocmd({ "VimResized" }, {
-  group = vim.api.nvim_create_augroup("AndoWinResize", { clear = true }),
-  callback = function()
-    vim.cmd "tabdo wincmd ="
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-  group = vim.api.nvim_create_augroup("AndoCRO", { clear = true }),
-  callback = function()
-    vim.cmd "set formatoptions-=cro"
   end,
 })
 
