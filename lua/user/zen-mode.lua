249d0f9615dc16e5a558bcab944ed152436e3e78
Andoni Arbulu Lozano <andoni0305@gmail.com>
2024-08-15 13:54:13 +1200

    chore(zen-mode): tweaks

--- lua/user/zen-mode.lua
+++ lua/user/zen-mode.lua
@@ -8,7 +8,7 @@ end
 zen.setup {
   window = {
     backdrop = 1, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
-    width = 120, -- width of the Zen window
+    width = 140, -- width of the Zen window
     height = 1, -- height of the Zen window
     -- by default, no options are changed for the Zen window
     -- uncomment any of the options below, or add other vim.wo options you want to apply
@@ -20,6 +20,7 @@ zen.setup {
       cursorcolumn = false, -- disable cursor column
       foldcolumn = "0", -- disable fold column
       list = false, -- disable whitespace characters
+      -- laststatus = 0,
     },
   },
   plugins = {
