7f05bb9615e6c82ee73f19c56cd406d791323364
Andoni Arbulu Lozano <andoni0305@gmail.com>
2024-08-16 15:03:02 +1200

    chore(cmp): cleanup

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -10,26 +10,8 @@ end
 
 local compare = require "cmp.config.compare"
 
-local check_backspace = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-end
-
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-    return false
-  end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match "^%s*$" == nil
-end
-
-local icons = require "user.icons"
-
--- local kind_icons = icons.kind
-
 -- NOTE: load snippets
 require("luasnip.loaders.from_vscode").lazy_load()
-
 cmp.setup {
   snippet = {
     expand = function(args)
@@ -38,22 +20,8 @@ cmp.setup {
   },
   preselect = cmp.PreselectMode.None,
   mapping = cmp.mapping.preset.insert {
-    ["<C-j>"] = vim.schedule_wrap(function(fallback)
-      if cmp.visible() and has_words_before() then
-        cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
-      else
-        fallback()
-      end
-    end),
-    ["<C-k>"] = vim.schedule_wrap(function(fallback)
-      if cmp.visible() and has_words_before() then
-        cmp.select_prev_item { behavior = cmp.SelectBehavior.Select }
-      else
-        fallback()
-      end
-    end),
-    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+    ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+    ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-c>"] = cmp.mapping {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
@@ -64,40 +32,9 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(1) then
-        luasnip.jump(1)
-      elseif luasnip.expandable() then
-        luasnip.expand()
-      elseif check_backspace() then
-        fallback()
-      else
-        fallback()
-      end
-    end, {
-      "n",
-      "i",
-      "s",
-    }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, {
-      "n",
-      "i",
-      "s",
-    }),
   },
   formatting = {
     fields = { "abbr", "kind", "menu" },
-    -- format = function(entry, vim_item)
-    --   -- Kind icons
-    --   vim_item.kind = kind_icons[vim_item.kind]
-    --   return vim_item
-    -- end,
     format = require("lspkind").cmp_format {
       mode = "symbol",
       maxwidth = 50,
@@ -115,10 +52,6 @@ cmp.setup {
       name = "buffer",
       group_index = 1,
     },
-    -- Copilot Source
-    { name = "copilot", group_index = 2 },
-    -- Codeium
-    { name = "codeium", group_index = 2 },
     { name = "nvim_lua", group_index = 2 },
     { name = "path", group_index = 2 },
     { name = "emoji", group_index = 3 },
@@ -127,12 +60,10 @@ cmp.setup {
     priority_weight = 2,
     comparators = {
       compare.exact,
-      compare.offset,
       compare.scopes,
       compare.locality,
+      compare.offset,
       compare.kind,
-      require("copilot_cmp.comparators").prioritize,
-      require("copilot_cmp.comparators").score,
       compare.recently_used,
       compare.score,
       compare.sort_text,
