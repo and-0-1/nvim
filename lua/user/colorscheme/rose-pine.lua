ec166301b4c5462709ea5932949897aca059d29e
Andoni Arbulu Lozano <andoni0305@gmail.com>
2024-10-03 09:29:43 +1300

    chore(rosepine): try out default

--- lua/user/colorscheme/rose-pine.lua
+++ lua/user/colorscheme/rose-pine.lua
@@ -4,39 +4,39 @@ if not status_ok then
 end
 
 rp.setup {
-  -- variant = "moon",
-  dim_nc_background = false,
-  disable_background = false,
-  disable_float_background = false,
-
-  styles = {
-    italic = true,
-  },
-
-  highlight_groups = {
-    Whitespace = { fg = "overlay" },
-    NonText = { fg = "overlay" },
-    CursorLine = { bg = "surface" },
-    ["@variable"] = { fg = "text", italic = false },
-    -- ["@variable.parameter"] = { fg = "iris", italic = false },
-    ["@property"] = { fg = "foam", italic = false },
-    Winbar = { fg = "iris", bg = "base" },
-    TelescopeBorder = { fg = "highlight_high", bg = "none" },
-    TelescopeNormal = { bg = "none" },
-    TelescopePromptNormal = { bg = "base" },
-    TelescopeResultsNormal = { fg = "subtle", bg = "none" },
-    TelescopeSelection = { fg = "text", bg = "base" },
-    -- TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
-    NormalFloat = { bg = "base" },
-    FloatBorder = { bg = "base", fg = "iris" },
-    WhichkeyFloat = { bg = "base" },
-    LspFloatWinNormal = { bg = "base" },
-    FloatShadow = { bg = "base" },
-    FloatShadowThrough = { bg = "base" },
-    MatchParen = { fg = "love", bold = true },
-    CursorLineNr = { fg = "pine", bold = true },
-    LineNr = { fg = "subtle" },
-  },
+  -- -- variant = "moon",
+  -- dim_nc_background = false,
+  -- disable_background = false,
+  -- disable_float_background = false,
+  --
+  -- styles = {
+  --   italic = true,
+  -- },
+  --
+  -- highlight_groups = {
+  --   Whitespace = { fg = "overlay" },
+  --   NonText = { fg = "overlay" },
+  --   CursorLine = { bg = "surface" },
+  --   ["@variable"] = { fg = "text", italic = false },
+  --   -- ["@variable.parameter"] = { fg = "iris", italic = false },
+  --   ["@property"] = { fg = "foam", italic = false },
+  --   Winbar = { fg = "iris", bg = "base" },
+  --   TelescopeBorder = { fg = "highlight_high", bg = "none" },
+  --   TelescopeNormal = { bg = "none" },
+  --   TelescopePromptNormal = { bg = "base" },
+  --   TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+  --   TelescopeSelection = { fg = "text", bg = "base" },
+  --   -- TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+  --   NormalFloat = { bg = "base" },
+  --   FloatBorder = { bg = "base", fg = "iris" },
+  --   WhichkeyFloat = { bg = "base" },
+  --   LspFloatWinNormal = { bg = "base" },
+  --   FloatShadow = { bg = "base" },
+  --   FloatShadowThrough = { bg = "base" },
+  --   MatchParen = { fg = "love", bold = true },
+  --   CursorLineNr = { fg = "pine", bold = true },
+  --   LineNr = { fg = "subtle" },
+  -- },
 }
 
 local colorscheme = "rose-pine"
