a6292a03e73a8debce8b62b64326638dd7f32e52
Andoni Arbulu Lozano <andoni0305@gmail.com>
2025-02-25 10:22:41 +1300

    chore(colorscheme): cleanup

--- lua/user/colorscheme/init.lua
+++ lua/user/colorscheme/init.lua
@@ -1,7 +1,3 @@
 -- colorscheme settings
 require "user.colorscheme.rose-pine"
--- require "user.colorscheme.mellow"
 -- require "user.colorscheme.tokyonight"
--- require "user.colorscheme.catpuccin"
--- require "user.colorscheme.melange"
--- require "user.colorscheme.nordic"
