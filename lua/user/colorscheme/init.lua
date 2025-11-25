6b9708f52fdba8a0520ac89260cce04b2058a8a9
Ando <andoni0305@gmail.com>
2025-11-25 17:15:59 +1300

    chore(colorscheme): line and cursors custom highlights

--- lua/user/colorscheme/init.lua
+++ lua/user/colorscheme/init.lua
@@ -1,3 +1,16 @@
 -- colorscheme settings
 require "user.colorscheme.rose-pine"
 -- require "user.colorscheme.tokyonight"
+
+local blueHighlight = {
+  bg = "#0000af", --[[ fg = "#ffffff" ]]
+}
+-- NOTE(ando): cursor color is controlled by kitty conf. But we put it here
+-- just for completeness
+local cursor_colors = {
+  bg = "#40FF40",
+  fg = "#000000",
+}
+vim.api.nvim_set_hl(0, "Visual", blueHighlight)
+vim.api.nvim_set_hl(0, "CursorLine", blueHighlight)
+vim.api.nvim_set_hl(0, "Cursor", cursor_colors)
