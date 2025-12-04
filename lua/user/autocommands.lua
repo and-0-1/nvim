91bbd4f465fd4bd0ca6b94d217e9db77c2aed751
Ando <andoni0305@gmail.com>
2025-12-04 17:12:05 +1300

    fix: do not add status line on everything

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -5,8 +5,8 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "DressingSelect" },
   callback = function()
     vim.cmd [[
-      nnoremap <silent> <buffer> q :close<CR> 
-      set nobuflisted 
+      nnoremap <silent> <buffer> q :close<CR>
+      set nobuflisted
     ]]
   end,
 })
@@ -21,6 +21,10 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   group = vim.api.nvim_create_augroup("AndoGitBranch", { clear = true }),
   callback = function()
+    if vim.bo.filetype == nil or vim.bo.filetype == "" then
+      return
+    end
+
     local current_file = vim.fn.expand "%:p"
     local branch = require("user.git_utils").get_git_branch(current_file)
 
