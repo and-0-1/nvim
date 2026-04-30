da7aee57c0576c864de020abc8d9d45386ba5d15
Ando <andoni0305@gmail.com>
2026-05-01 08:42:03 +1200

    chore(options): drop default values, dead comments, and obvious docstrings
    - numberwidth = 4 is the default
    - wildchar uses literal byte 14 (<C-n>) instead of a raw control char
      embedded in a string literal
    - strip per-option comments that just restate what the option name
      already says
    - delete the block of commented-out fillchars/shortmess/netrw config

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,63 +1,45 @@
 local statusline = require "user.statusline"
 local options = {
-  backup = false, -- creates a backup file
-  clipboard = "unnamedplus", -- allows neovim to access the system clipboard
-  cmdheight = 1, -- more space in the neovim command line for displaying messages
-  completeopt = { "menuone", "noselect", "fuzzy", "popup" }, -- mostly just for cmp
+  backup = false,
+  clipboard = "unnamedplus",
+  cmdheight = 1,
+  completeopt = { "menuone", "noselect", "fuzzy", "popup" },
   complete = ".,w,b,u,t,o",
-  conceallevel = 0, -- so that `` is visible in markdown files
-  fileencoding = "utf-8", -- the encoding written to a file
-  hlsearch = true, -- highlight all matches on previous search pattern
-  ignorecase = true, -- ignore case in search patterns
-  -- mouse = "a", -- allow the mouse to be used in neovim
-  -- pumheight = 10, -- pop up menu height
-  showtabline = 1, -- always show tabs
-  smartcase = true, -- smart case
+  conceallevel = 0,
+  fileencoding = "utf-8",
+  hlsearch = true,
+  ignorecase = true,
+  showtabline = 1,
+  smartcase = true,
   smartindent = true,
   autoindent = true,
-  splitbelow = true, -- force all horizontal splits to go below current window
-  splitright = true, -- force all vertical splits to go to the right of current window
-  swapfile = false, -- creates a swapfile
-  termguicolors = true, -- set term gui colors (most terminals support this)
-  timeoutlen = 500, -- time to wait for a mapped sequence to complete (in milliseconds)
-  undofile = true, -- enable persistent undo
-  updatetime = 1000, -- faster completion (4000ms default)
-  writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  expandtab = true, -- convert tabs to spaces
-  shiftwidth = 2, -- the number of spaces inserted for each indentation
-  tabstop = 2, -- insert 2 spaces for a tab
-  cursorline = true, -- highlight the current line
-  number = true, -- set numbered lines
-  relativenumber = true, -- set relative numbered lines
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  termguicolors = true,
+  timeoutlen = 500,
+  undofile = true,
+  updatetime = 1000,
+  writebackup = false,
+  expandtab = true,
+  shiftwidth = 2,
+  tabstop = 2,
+  cursorline = true,
+  number = true,
+  relativenumber = true,
   statusline = statusline.full,
   laststatus = 2,
-  numberwidth = 4, -- set number column width to 2 {default 4}
-  signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
-  wrap = false, -- wrap lines
-  scrolloff = 8, -- is one of my fav
+  signcolumn = "yes",
+  wrap = false,
+  scrolloff = 8,
   sidescrolloff = 8,
-  -- guifont = "monospace:h17", -- the font used in graphical neovim applications
-  -- listchars = "tab:→ ,trail:·,eol:↲,extends:…,precedes:…",
   listchars = "tab:→ ,trail:·,lead:·,extends:…,precedes:…,eol:↵",
   list = false,
   foldmethod = "marker",
-  wildchar = vim.fn.char2nr "", -- change autocomplete menu trigger from Tab -> Ctrl - n; wildchar only accepts number in lua, so we transform char to number
+  -- <C-n> as autocomplete menu trigger instead of <Tab>; wildchar wants a number
+  wildchar = 14,
   winborder = "rounded",
-  -- syntax = "off"
 }
-
--- vim.opt.fillchars = vim.opt.fillchars + "vertleft: "
--- vim.opt.fillchars = vim.opt.fillchars + "vertright: "
-
--- vim.opt.fillchars = vim.opt.fillchars + "eob: "
--- vim.opt.fillchars:append {
---   stl = " ",
--- }
-
--- vim.opt.shortmess:append "c"
-
--- vim.g.netrw_keepdir = 0
--- vim.g.netrw_liststyle = 3 -- show tree style by default
 
 for k, v in pairs(options) do
   vim.opt[k] = v
