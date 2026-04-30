2cf10b5a5658fa71a28d42cfd8b61509d4d381a3
Ando <andoni0305@gmail.com>
2026-05-01 09:18:33 +1200

    perf(bigfile): kill matchpairs, matchparen, cursorline, relativenumber
    Adds mitigations for what was still slow on >512KB buffers:
    - matchpairs = '' disables % motion (so it can't hang on giant lines)
    - b:matchparen_disabled tells the built-in matchparen plugin to skip
    - synmaxcol = 200 caps regex highlighting per line
    - cursorline and relativenumber both redraw on every motion

--- lua/user/bigfile.lua
+++ lua/user/bigfile.lua
@@ -11,14 +11,19 @@ vim.api.nvim_create_autocmd("BufReadPre", {
     end
 
     vim.b[ev.buf].large_buf = true
+    vim.b[ev.buf].matchparen_disabled = 1
 
     vim.bo[ev.buf].swapfile = false
     vim.bo[ev.buf].undofile = false
     vim.bo[ev.buf].syntax = ""
+    vim.bo[ev.buf].matchpairs = ""
+    vim.bo[ev.buf].synmaxcol = 200
 
     vim.opt_local.foldmethod = "manual"
     vim.opt_local.list = false
     vim.opt_local.spell = false
+    vim.opt_local.cursorline = false
+    vim.opt_local.relativenumber = false
   end,
 })
 
