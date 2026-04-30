6c54d79bca140f2d414988ed4f438ce32fa0528a
Ando <andoni0305@gmail.com>
2026-05-01 08:33:08 +1200

    chore(handlers): drop redundant snippetSupport and built-in K hover keymap

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -8,7 +8,6 @@ if not status_cmp_ok then
   return
 end
 
-M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
 
 M.setup = function()
@@ -35,14 +34,11 @@ M.setup = function()
   vim.diagnostic.config(config)
 end
 
--- TODO(ando): refactor this to be simpler, new nvim lsp api supports a lot of the capabilities we need
 local function lsp_keymaps(bufnr)
-  local opts = { noremap = true, silent = true }
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover({border = 'rounded'})<CR>", opts)
   vim.keymap.set("n", "<leader>lv", function()
     local new_config = not vim.diagnostic.config().virtual_lines
     vim.diagnostic.config { virtual_lines = new_config }
-  end, { desc = "Toggle diagnostic virtual_lines", buffer = true })
+  end, { desc = "Toggle diagnostic virtual_lines", buffer = bufnr })
 end
 
 M.on_attach = function(client, bufnr)
