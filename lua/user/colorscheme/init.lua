d22f53c726951e4d0b06229b0194dc320392876f
Ando <andoni0305@gmail.com>
2026-05-10 10:06:05 +1200

    docs(colorscheme): tidy up

--- lua/user/colorscheme/init.lua
+++ lua/user/colorscheme/init.lua
@@ -12,8 +12,8 @@ vim.api.nvim_create_autocmd("OptionSet", {
 -- local blueHighlight = {
 --   bg = "#0000af", --[[ fg = "#ffffff" ]]
 -- }
--- NOTE(ando): cursor color is controlled by kitty conf. But we put it here
--- just for completeness
+-- NOTE(ando): cursor color is controlled by the terminal config (ghostty/
+-- alacritty). Kept here just for completeness.
 -- local cursor_colors = {
 --   bg = "#40FF40",
 --   fg = "#000000",
